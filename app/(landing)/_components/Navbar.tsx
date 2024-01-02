39cf5038117c1e407d56e90219c5bcc6239ddaa0
abudusamad <abudusamed@gmail.com>
2024-01-02 20:41:52 +0000

    Making the notion navbar for the dashboard

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -1,26 +1,31 @@
 "use client";
 
+import useScrollTop from "@/app/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/media-toggle";
 import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { useConvexAuth } from "convex/react";
 import Link from "next/link";
 import { Logo } from "./Logo";
-import { ModeToggle } from "@/components/media-toggle";
-import useScrollTop from "@/app/hooks/use-scroll-top";
-import { cn } from "@/lib/utils";
 
 const Navbar = () => {
 	const { isAuthenticated, isLoading } = useConvexAuth();
 	const scrolled = useScrollTop();
-	
+
 	return (
-		<div className={cn("z-50 bg-background fixed top-0 flex items-center w-full dark:bg-[#1F1F1F]", scrolled && "border-b shadow-sm")}>
+		<div
+			className={cn(
+				"z-50 bg-background fixed top-0 flex items-center justify-between w-full dark:bg-[#1F1F1F]",
+				scrolled && "border-b shadow-sm"
+			)}
+		>
 			<Logo />
 			<div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
 				{isLoading && <Spinner />}
 				{!isLoading && !isAuthenticated && (
-					<>
+					<div className={cn(scrolled && "hidden")}>
 						<SignInButton mode="modal">
 							<Button variant="ghost" size="sm">
 								Log in
@@ -31,7 +36,7 @@ const Navbar = () => {
 								Get Notion free
 							</Button>
 						</SignInButton>
-					</>
+					</div>
 				)}
 				{!isLoading && isAuthenticated && (
 					<>
