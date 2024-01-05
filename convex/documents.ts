f91c152a649336973febfa8eb1869fe4c91323a6
abudusamad <abudusamed@gmail.com>
2024-01-05 12:59:31 +0000

    Making the document page

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,38 +1,35 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const get = query({
-    handler: async (ctx) => {
-        const tasks = await ctx.db.query("documents").collect();    
-        return tasks
-    }
-   
-})
-
-
+	handler: async (ctx) => {
+		const tasks = await ctx.db.query("documents").collect();
+		return tasks;
+	},
+});
 
 export const create = mutation({
 	args: {
 		title: v.string(),
 		parentDocumet: v.optional(v.id("documents")),
 	},
+
 	handler: async (ctx, args) => {
 		const identity = await ctx.auth.getUserIdentity();
 
 		if (!identity) {
 			throw new Error("Not authenticated");
-        }
-        
-        const userId = identity.subject;
+		}
 
-        const documents = await ctx.db.insert("documents", {
-            title: args.title,
-            parentDocument: args.parentDocumet,
-            userId,
-            isArchived: false,
-            isPublished: false,
-        });
-        return documents;
+		const userId = identity.subject;
+
+		const documents = await ctx.db.insert("documents", {
+			title: args.title,
+			parentDocument: args.parentDocumet,
+			userId,
+			isArchived: false,
+			isPublished: false,
+		});
+		return documents;
 	},
 });
