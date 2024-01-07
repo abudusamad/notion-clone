34a15369fdfa19b26e250ed9819304a74cd9546b
abudusamad <abudusamed@gmail.com>
2024-01-07 18:26:32 +0000

    making the items Page

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -16,9 +16,17 @@ export const Item = ({
    
     return (
         <div
-        style={{paddingLeft: "12px"}}
+            style={{ paddingLeft: "12px" }}
+            className="group min-h-[27px] text-sm pr-3 py-1 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium
+            "
         >
-            Item List
+            <div className="h-full flex items-center gap-x-2 rounded-sm hover:bg-neutral-300 mr-1">
+                <Icon className="shrink-0 h-[18px] w-[18px] text-muted-foreground" />
+                <span className="truncate">
+                    {label}
+                </span>
+                
+          </div>
         </div>
     )
 }
