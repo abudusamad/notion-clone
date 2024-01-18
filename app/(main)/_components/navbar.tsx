ae8f0303c4686b5ec5c6bb7461ae08a0d6eabc78
abudusamad <abudusamed@gmail.com>
2024-01-18 23:56:10 +0000

    Making the menubar for the page

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { MenuIcon } from "lucide-react";
 import { Title } from "./title";
 import { Menu } from "./menu";
+import { useQueries, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface NavbarProps {
 	isCollapsed: boolean;
@@ -8,6 +14,24 @@ interface NavbarProps {
 }
 
 const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+	const params = useParams();
+
+	const document = useQuery(api.documents.getById, {
+		documentId: params.documentId as Id<"documents">,
+	})
+
+	if (document === undefined) {
+		return (
+			<nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+				<div className="flex items-center gap-x-2">
+					<Menu.Skeleton/>
+				</div>
+			</nav>
+		)
+	}
+	if (document === null) {
+		return null;
+	}
 	return (
 		<nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
 			{isCollapsed && (
@@ -20,7 +44,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 			<div className="flex items-center justify-between w-full">
 				<Title/>
 				<div className="flex items-center gap-x-2">
-					<Menu/>
+					<Menu documentId={document?._id}/>
 				</div>
 			</div>
 		</nav>
--- convex/documents.ts
+++ convex/documents.ts
@@ -198,3 +198,30 @@ export const restore = mutation({
 		recursiveRestore(args.id);
 	},
 });
+
+export const getById = query({
+	args: { documentId: v.id("documents") },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+
+		if (!identity) {
+			throw new Error("Not authenticated");
+		}
+
+		const userId = identity.subject;
+
+		const document = await ctx.db.get(args.documentId);
+		if (document?.isPublished && !document.isArchived) {
+			return document;
+		}
+
+		if (!document) {
+			throw new Error("Document not found");
+		}
+		if (document.userId !== userId) {
+			throw new Error("Unauthorized");
+		}
+
+		return document;
+	},
+})
