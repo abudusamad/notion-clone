1eb2bd8142b932da9d3e1470ac20ccb42bccd330
abudusamad <abudusamed@gmail.com>
2024-01-28 18:24:27 +0000

    Making the page

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { ImageIcon, X } from "lucide-react";
-import Image from "next/image";
-import { Button } from "./ui/button";
-import { cn } from "@/lib/utils";
 import { useCoverImage } from "@/app/hooks/use-cover-image";
-import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useParams } from "next/navigation";
 import { useEdgeStore } from "@/lib/edgestore";
+import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
+import { ImageIcon, X } from "lucide-react";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { Button } from "./ui/button";
 
 interface CoverImageProps {
 	url?: string;
@@ -17,34 +17,34 @@ interface CoverImageProps {
 }
 
 export const CoverImage = ({ url, preview }: CoverImageProps) => {
-    const {edgestore} = useEdgeStore();
-    const coveImage = useCoverImage();
-    const removeCoverImage = useMutation(api.documents.removeCoverImage);
-    const params = useParams();
+	const { edgestore } = useEdgeStore();
+	const coveImage = useCoverImage();
+	const removeCoverImage = useMutation(api.documents.removeCoverImage);
+	const params = useParams();
 
-    
-
-    const onRemove = async () => {
-        if (url) {
-            await edgestore.publicFiles.delete({
-                url: url
-           })
-       }
-        removeCoverImage({id: params.documentId as Id<"documents">})
-    }
+	const onRemove = async () => {
+		removeCoverImage({ id: params.documentId as Id<"documents"> });
+		if (url) {
+			await edgestore.publicFiles.delete({
+				url: url,
+			});
+		}
+	};
 	return (
-        <div className={cn("relative w-full h-[35vh] group",
-            !url && "h-[8vh]",
-            url && "bg-muted"
-            
-        )}>
+		<div
+			className={cn(
+				"relative w-full h-[35vh] group",
+				!url && "h-[8vh]",
+				url && "bg-muted"
+			)}
+		>
 			{!!url && (
 				<Image src={url} alt="Cover Image" className="object-cover " fill />
 			)}
 			{!!url && preview && (
 				<div className="opacity-0 group-hover:opacity-100 absolute top-16 right-1/4 flex items-center  ">
-                    <Button
-                    onClick={()=>coveImage.onReplace(url)}
+					<Button
+						onClick={() => coveImage.onReplace(url)}
 						variant="outline"
 						size="sm"
 						className="text-muted-foreground tex-xs"
@@ -52,8 +52,8 @@ export const CoverImage = ({ url, preview }: CoverImageProps) => {
 						<ImageIcon className="w-4 h-4 mr-2" />
 						Change Cover
 					</Button>
-                    <Button
-                        onClick={onRemove}
+					<Button
+						onClick={onRemove}
 						variant="outline"
 						size="sm"
 						className="text-muted-foreground tex-xs"
