407175fb684d335602d67d098ecd2b235f9f4111
abudusamad <abudusamed@gmail.com>
2024-01-30 23:15:06 +0000

    Adding the useMemo to memoraise so that is calculated once on render

--- app/hooks/use-origin.tsx
+++ app/hooks/use-origin.tsx
@@ -1,21 +1,10 @@
-"use client";
+import { useMemo } from "react";
 
-import { useEffect, useState } from "react";
-
-export const Origin = () => {
-	const [isMounted, setIsMounted] = useState(false);
-
-	const origin =
-		typeof window !== "undefined" && window.location.origin
+export const useOrigin = () => {
+	const origin = useMemo(() => {
+		return typeof window !== "undefined" && window.location.origin
 			? window.location.origin
 			: "";
-
-	useEffect(() => {
-		setIsMounted(true);
 	}, []);
-
-	if (!isMounted) {
-		return "";
-	}
 	return origin;
 };
