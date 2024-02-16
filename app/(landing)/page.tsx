66bf5838c31ebe77ec1713d3a499de22a7c1a852
abudusamad <abudusamed@gmail.com>
2024-02-16 00:10:41 +0000

    new feature to  the landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,8 +5,15 @@ import Hero from "./_components/Hero";
 
 const LandingPage = () => {
 	return (
-		<div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
-			<div className="flex flex-col items-center justify-center gap-y-8 flex-1 px-6 pb-10">
+		<div className="min-h-screen md:min-h-[83vh] flex flex-col">
+			<div className="mt-32 md:mt-0 text-3xl sm:text-center-5xl md:text-6xl lg:text-8xl text-center">
+				<h1 className="mt-32 md:mt-0 text-3xl sm:text-5xl md:text-6xl lg:text-8xl text-center font-switzerBold">
+					Make.{" "}
+					<span className="bg-gradient-to-r bg-clip-text text-transparent from-yellow-500 via-purple-500 to-red-500 animate-text">
+						Dreams.
+					</span>{" "}
+					Happen.
+				</h1>
 				<Heading />
 				<Hero />
 			</div>
