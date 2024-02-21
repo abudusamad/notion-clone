64377466c604ac21b7794a7db56246ef93b9aad7
abudusamad <abudusamed@gmail.com>
2024-02-21 01:07:45 +0000

    Making the chat schema

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -10,3 +10,44 @@ export const getSpaceNameFromUrl = () => {
 		return documentId;
 	}
 };
+
+export const REMOVE_USER_AFTER_MILLIS = 120_000;
+export const MAX_USRS_BEFORE_LIST = 5;
+
+export type Member = {
+	userId: string;
+	lastActive: number;
+	location: string;
+	userPicture: string;
+	userName: string;
+	lastSeen: number;
+};
+
+export type Message = {
+	_creationTime: number;
+	_id: string;
+	documentId: string;
+	message: string;
+	userId: string;
+	userName: string;
+	isArchived: boolean;
+};
+
+export type HomeChat = {
+	_creationTime: number;
+	_id: string;
+	message: string;
+	userId: string;
+	userName: string;
+	isArchived: boolean;
+};
+
+export const getIntials = (name: string) => {
+	const words = name.split(" ");
+	if (words.length === 1) {
+		return words[0].charAt(0);
+	}
+	const firstLetter = words[0].charAt(0);
+	const lastLetter = words[words.length - 1].charAt(0);
+	return `${firstLetter}${lastLetter}`;
+};
