9a817b0012af609c84a70ddabf7d9ca3c733498b
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-05-06 22:19:49 +0000

    Fix: Employ Metadata to the application

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,10 +6,14 @@ import { Spinner } from "@/components/spinner";
 import { UpdatePopover } from "@/components/update-popover";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
-import { Navigation } from "./_components/navigation";
 import { getSpaceNameFromUrl } from "../utils/helpers";
+import { Navigation } from "./_components/navigation";
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+interface MainLayoutProps {
+	children: React.ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
 	const { isAuthenticated, isLoading } = useConvexAuth();
 
 	if (isLoading) {
@@ -25,10 +29,9 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 	const spaceName = getSpaceNameFromUrl();
 	console.log(spaceName);
 
-
 	return (
 		<>
-			<AvatarCard nameOfSpace={ spaceName??"documents"} />
+			<AvatarCard nameOfSpace={spaceName ?? "documents"} />
 			<div className="h-full flex  dark:bg-[#1F1F1F]">
 				<Navigation />
 				<main className="flex-1 h-full overflow-y-auto">
