48637cc9ff6f65760f0927dbe3c4fb1019e5a4c6
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-05-06 22:25:40 +0000

    Fixed : Dynamic import

--- app/(main)/(route)/documents/[documentId]/page.tsx
+++ app/(main)/(route)/documents/[documentId]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { CoverImage } from "@/components/cover-image";
-import { Editor } from "@/components/editor";
 import { Toolbar } from "@/components/toolbar";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 interface DocumentIdPageProps {
 	params: {
@@ -14,10 +15,10 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
-	// const Editor = useMemo(
-	// 	() => dynamic(() => import("@/components/editor"), { ssr: false }),
-	// 	[]
-	// );
+	const Editor = useMemo(
+		() => dynamic(() => import("@/components/editor").then(mod=>mod.Editor), { ssr: false }),
+		[]
+	);
 
 	const document = useQuery(api.documents.getById, {
 		documentId: params.documentId,
