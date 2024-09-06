0a53257314288a724496cffaaed37b57904f7467
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-09-06 23:57:50 +0000

    Adding the credential login to the convex

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+import { SignInCard } from "@/components/features/auth/components/sign-in-card";
+
+const SignIn = () => {
+    return (
+        <SignInCard />
+    );
+}
+ 
+export default SignIn;
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+
+
+const SignUp = () => {
+    return (
+        <div>
+            This is the sign up page
+        </div>
+    )
+}
+ 
+export default SignUp;
