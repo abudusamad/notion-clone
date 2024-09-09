ba522f4b31be1fe84f235e7fc7c0a91094b8b1aa
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-09-09 09:50:14 +0000

    Adding the google-authentication

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,9 +1,13 @@
 import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
  
 export const { auth, signIn, signOut, store } = convexAuth({
   providers: [GitHub({
     clientId: process.env.GITHUB_CLIENT_ID!,
     clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+  }), Google({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
   })],
 });
