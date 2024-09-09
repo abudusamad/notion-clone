bf9d1a1abc157df6855a732e9f61c08e20309a74
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-09-09 10:24:52 +0000

    Adding authentication to the application

--- app/(landing)/_components/Heading.tsx
+++ app/(landing)/_components/Heading.tsx
@@ -1,67 +1,63 @@
 "use client";
-import { useConvexAuth } from "convex/react";
+import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
 import { ArrowRight } from "lucide-react";
-import { SignInButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
 
 export const Heading = () => {
-	const { isAuthenticated, isLoading } = useConvexAuth();
-	const date = new Date();
+  const date = new Date();
 
-	return (
-		<div className="max-w-3xl space-y-4 mt-3 md:text-left">
-			<h1 className="text-6xl sm:text-xl md:text-3xl font-switzerSemibold">
-				Notion Extended is a{" "}
-				<span className="bg-gradient-to-r bg-clip-text text-transparent from-yellow-500 via-purple-500 to-red-500 animate-text">
-					realtime collaborative
-				</span>
-				&nbsp; workspace
-			</h1>
-			<h3 className="text-4xl sm:text-xl md:text-3xl font-switzerMedium ">
-				Notion Extended is my submission for <br />
-				<a
-					href="https://stack.convex.dev/build-bounty-jotion/"
-					target="_blank"
-					className="hover:cursor-pointer bg-gradient-to-r bg-clip-text text-transparent from-blue-100 via-purple-500 to-red-900 animate-text"
-				>
-					Convex Build Bounty {date.getFullYear()}
-				</a>
-			</h3>
-			<h3 className="text-3xl sm:text-xl md:text-2xl font-switzerMedium">
-				Notion Extended is made possible by <br />
-				<span className="bg-gradient-to-r bg-clip-text text-transparent from-yellow-500 via-purple-500 to-red-500 animate-text">
-					Convex
-				</span>{" "}
-				and more
-			</h3>
-			{isLoading && (
-				<div className="w-full flex items-center justify-center">
-					<Spinner size="lg" />
-				</div>
-			)}
-			{isAuthenticated && !isLoading && (
-				<Button
-					asChild
-					variant="outline"
-					className="bg-inherit border-indigo-500 rounded-md"
-				>
-					<Link href="/documents">
-						Enter Notion Extended
-						<ArrowRight className="h-4 w-4 ml-2" />
-					</Link>
-				</Button>
-			)}
-			{!isAuthenticated && !isLoading && (
-				<SignInButton mode="modal">
-					<Button >
-						Get Jotion Extended free
-						<ArrowRight className="h-4 w-4 ml-2" />
-					</Button>
-				</SignInButton>
-			)}
-		</div>
-	);
+  return (
+    <div className="max-w-3xl space-y-4 mt-3 md:text-left">
+      <h1 className="text-6xl sm:text-xl md:text-3xl font-switzerSemibold">
+        Notion Extended is a{" "}
+        <span className="bg-gradient-to-r bg-clip-text text-transparent from-yellow-500 via-purple-500 to-red-500 animate-text">
+          realtime collaborative
+        </span>
+        &nbsp; workspace
+      </h1>
+      <h3 className="text-4xl sm:text-xl md:text-3xl font-switzerMedium ">
+        Notion Extended is my submission for <br />
+        <a
+          href="https://stack.convex.dev/build-bounty-jotion/"
+          target="_blank"
+          className="hover:cursor-pointer bg-gradient-to-r bg-clip-text text-transparent from-blue-100 via-purple-500 to-red-900 animate-text"
+        >
+          Convex Build Bounty {date.getFullYear()}
+        </a>
+      </h3>
+      <h3 className="text-3xl sm:text-xl md:text-2xl font-switzerMedium">
+        Notion Extended is made possible by <br />
+        <span className="bg-gradient-to-r bg-clip-text text-transparent from-yellow-500 via-purple-500 to-red-500 animate-text">
+          Convex
+        </span>{" "}
+        and more
+      </h3>
+      <AuthLoading>
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      </AuthLoading>
+
+      <Authenticated>
+        <Button
+          asChild
+          variant="outline"
+          className="bg-inherit border-indigo-500 rounded-md"
+        >
+          <Link href="/documents">
+            Enter Notion Extended
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+		  </Authenticated>
+		  
+        <Button variant="outline">
+          <Link href="/signin">Sign In</Link>
+        </Button>
+    
+    </div>
+  );
 };
--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -5,53 +5,53 @@ import { ModeToggle } from "@/components/media-toggle";
 import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { useConvexAuth } from "convex/react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 import Link from "next/link";
 import { Logo } from "./Logo";
 
 const Navbar = () => {
-	const { isAuthenticated, isLoading } = useConvexAuth();
-	const scrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
+  const { signOut } = useAuthActions();
 
-	return (
-		<div
-			className={cn(
-				"z-50 bg-background fixed top-0 flex items-center justify-between w-full dark:bg-[#1F1F1F]",
-				scrolled && "border-b shadow-sm"
-			)}
-		>
-			<Logo />
-			<div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-				{isLoading && <Spinner />}
-				{!isLoading && !isAuthenticated && (
-					<div className={cn(scrolled && "hidden")}>
-						<SignInButton mode="modal">
-							<Button variant="ghost" size="sm">
-								Log in
-							</Button>
-						</SignInButton>
-						<SignInButton mode="modal">
-							<Button size="sm" className="mr-2">
-								Get Notion free
-							</Button>
-						</SignInButton>
-					</div>
-				)}
-				{!isLoading && isAuthenticated && (
-					<>
-						<Button>
-							<Link href="/documents">Enter Notion</Link>
-						</Button>
-						<UserButton afterSignOutUrl="/" />
-					</>
-				)}
-				<div className="mr-2">
-					<ModeToggle />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex items-center justify-between w-full dark:bg-[#1F1F1F]",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isLoading && !isAuthenticated && (
+          <div className={cn(scrolled && "hidden")}>
+            <Button>
+              <Link href="/auth/signin">Sign In</Link>
+            </Button>
+            <Button onClick={() => signOut()} variant="ghost">
+              Sign Out
+            </Button>
+          </div>
+        )}
+        {!isLoading && isAuthenticated && (
+          <>
+            <Button>
+              <Link href="/documents">Documents</Link>
+            </Button>
+            <Button>
+              <Link href="/auth/signout">Sign Out</Link>
+            </Button>
+          </>
+        )}
+        <div className="mr-2">
+          <ModeToggle />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
