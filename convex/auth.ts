5ef23d1e7a0e7836ff8493098f74cbb563bfb577
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-09-09 22:58:11 +0000

    Adding some sessions

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,13 +1,20 @@
+import {Password} from "@convex-dev/auth/providers/Password";
 import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
  
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [GitHub({
+  providers:
+    [GitHub({
     clientId: process.env.GITHUB_CLIENT_ID!,
     clientSecret: process.env.GITHUB_CLIENT_SECRET!,
   }), Google({
     clientId: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  })],
+  }),
+  Password
+
+  ]
+  
+  ,
 });
